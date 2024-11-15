999dcf6e31dee0ebe769b3d9d9c09a14608c97ae
Ivan Došlić <ivan.doslic@fer.hr>
2024-11-15 17:56:56 +0100

    add remaining
    Co-Authored-By: lucija3462 <185216188+lucija3462@users.noreply.github.com>
    Co-Authored-By: dominikKos9 <185245924+dominikKos9@users.noreply.github.com>
    Co-Authored-By: dzankicivan <185543692+dzankicivan@users.noreply.github.com>

--- IzvorniKod/backend/src/main/java/hr/fer/styletrack/backend/dtos/ItemDto.java
+++ IzvorniKod/backend/src/main/java/hr/fer/styletrack/backend/dtos/ItemDto.java
@@ -12,13 +12,17 @@ public class ItemDto {
     private Long sectionId;
     private Long itemId;
     private String itemName;
+    private String category;
+    private String description;
     private Collection<String> itemTags;
 
-    public ItemDto(Long sectionId, Long itemId, String itemName, Collection<String> itemTags){
+    public ItemDto(Long sectionId, Long itemId, String itemName, Collection<String> itemTags, String description, String category) {
         this.sectionId = sectionId;
         this.itemId = itemId;
         this.itemName = itemName;
         this.itemTags = itemTags;
+        this.description = description;
+        this.category = category;
     }
 
     public ItemDto(Item item) {
@@ -26,13 +30,18 @@ public class ItemDto {
         this.itemId = item.getItemId();
         this.itemName = item.getItemName();
         this.itemTags = item.getItemTags();
+        this.description = item.getDescription();
+        this.category = item.getCategory();
     }
 
     @Override
     public String toString() {
         return "{"
-                + "        \"itemId\":\"" + itemId + "\""
+                + "        \"sectionId\":\"" + sectionId + "\""
+                + ",         \"itemId\":\"" + itemId + "\""
                 + ",         \"itemName\":\"" + itemName + "\""
+                + ",         \"categoryName\":\"" + category + "\""
+                + ",         \"description\":\"" + description + "\""
                 + ",         \"itemTags\":" + itemTags
                 + "}";
     }
